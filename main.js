7aa12c0130715ec9d673f28616c4d5b4737cb51d
kitazaki <kitazaki@gmail.com>
2018-07-01 21:25:57 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var milkcocoa = new MilkCocoa('flagj7ie632f.mlkcca.com');
+var chatDataStore = milkcocoa.dataStore("led");
+var board;
+window.onload = function(){
+  board = document.getElementById("board");
+}
+ 
+function clickEvent_one(){
+  var v_r = 1; var v_g = 1; var v_b = 1; var v_s = 1; var v_t = 1;
+  sendText(v_r, v_g, v_b, v_s, v_t);
+}
+
+function clickEvent_two(){
+  var v_r = 1; var v_g = 1; var v_b = 1; var v_s = 1; var v_t = 10;
+  sendText(v_r, v_g, v_b, v_s, v_t);
+}
+
+function sendText(v_r, v_g, v_b, v_s, v_t){
+  chatDataStore.push({r : v_r, g : v_g, b : v_b, s : v_s, t : v_t},function(data){
+    console.log("送信完了!");
+  });
+}
+ 
+chatDataStore.on("push",function(data){
+  addText(data.value.r, data.value.g, data.value.b, data.value.s, data.value.t);
+});
+ 
+function addText(v_r, v_g, v_b, v_s, v_t){
+  var msgDom = document.createElement("li");
+  msgDom.innerHTML = v_r + "," + v_g + "," + v_b + "," + v_s + "," + v_t;
+  board.insertBefore(msgDom, board.firstChild);
+}
+
